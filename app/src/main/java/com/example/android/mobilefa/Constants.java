eccb7a27ae2d0dcb5ddfc77c9f9f4713e2d0033c
EstherRuby21 <ruby7merlin@gmail.com>
2020-07-05 15:52:11 +0530

    Login, Register & Events

--- app/src/main/java/com/example/android/mobilefa/Constants.java
+++ app/src/main/java/com/example/android/mobilefa/Constants.java
@@ -1,7 +1,7 @@
 package com.example.android.mobilefa;
 
 public class Constants {
-    private static String url= "http://192.168.43.9:8080/Android/";
+    private static String url= "dupastone.bytehost15.com/test.php";
     public static String REGISTER_URL =  url + "register.php";
     public static  String LOGIN_URL = url + "login.php";
     public  static  String EVENT_URL = url + "insertevent.php";
