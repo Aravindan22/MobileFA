95cf0ed7406519f3441bb9dc299eeedbb145c0b2
EstherRuby21 <ruby7merlin@gmail.com>
2020-07-05 16:05:02 +0530

    Esther

--- app/src/main/java/com/example/android/mobilefa/Constants.java
+++ app/src/main/java/com/example/android/mobilefa/Constants.java
@@ -1,7 +1,7 @@
 package com.example.android.mobilefa;
 
 public class Constants {
-    private static String url= "dupastone.bytehost15.com/test.php";
+    private static String url= "http://dupastone.byethost15.com/Android/";
     public static String REGISTER_URL =  url + "register.php";
     public static  String LOGIN_URL = url + "login.php";
     public  static  String EVENT_URL = url + "insertevent.php";
