47b84a9de491ad6dcecc76ac283250d999864c6d
Aravindan22 <63189666+Aravindan22@users.noreply.github.com>
2020-07-06 10:19:13 +0530

    Updated Constants

--- app/src/main/java/com/example/android/mobilefa/Constants.java
+++ app/src/main/java/com/example/android/mobilefa/Constants.java
@@ -1,11 +1,16 @@
 package com.example.android.mobilefa;
 
 public class Constants {
-    private static String url= "http://dupastone.byethost15.com/Android/";
-    public static String REGISTER_URL =  url + "register.php";
-    public static  String LOGIN_URL = url + "login.php";
-    public  static  String EVENT_URL = url + "insertevent.php";
-    public static  String name;
-    public  static String regno;
-    public  static  String dep;
+    public static int REG_NO;
+    public static String PASSWORD;
+    public static String DEP;
+    public static String EMAIL;
+    public static String NAME;
+    public static int YEAR;
+    public static String SECTION;
+    private static String url = "http://dupastone.byethost15.com/Android/";
+    public static String REGISTER_URL = url + "register.php";
+    public static String LOGIN_URL = url + "login.php";
+    public static String EVENT_URL = url + "insertevent.php";
+    public static String FETCHDATA_URL = url + "retrieveCreds.php";
 }
