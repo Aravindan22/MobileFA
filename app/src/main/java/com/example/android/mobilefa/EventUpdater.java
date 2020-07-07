e411be4f8c1efbe318a54f3026e2c9236d84f0ad
Esther Ruby V <51705296+EstherRuby21@users.noreply.github.com>
2020-07-07 20:01:45 +0530

    Update EventUpdater.java

--- app/src/main/java/com/example/android/mobilefa/EventUpdater.java
+++ app/src/main/java/com/example/android/mobilefa/EventUpdater.java
@@ -1,13 +1,27 @@
 package com.example.android.mobilefa;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+//import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+/*import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.android.mobilefa.Constants.DEP;
+import static com.example.android.mobilefa.Constants.REG_NO;*/
 
 public class EventUpdater extends AppCompatActivity {
     Button workshop_btn, course_btn, comp_btn;
@@ -16,7 +30,7 @@ public class EventUpdater extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_updater);
 
@@ -26,7 +40,7 @@ public class EventUpdater extends AppCompatActivity {
 
         workshop_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent workshopIntent = new Intent(getApplicationContext(), WorkshopEvent.class);
                 startActivity(workshopIntent);
                 finish();
@@ -35,7 +49,7 @@ public class EventUpdater extends AppCompatActivity {
 
         course_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent courseIntent = new Intent(getApplicationContext(), CourseEvent.class);
                 startActivity(courseIntent);
                 finish();
@@ -44,13 +58,14 @@ public class EventUpdater extends AppCompatActivity {
 
         comp_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent compIntent = new Intent(getApplicationContext(), CompEvent.class);
                 startActivity(compIntent);
                 finish();
             }
         });
     }
+
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getApplicationContext(),ChoosingActivity.class));
